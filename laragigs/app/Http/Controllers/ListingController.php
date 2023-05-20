ad5755d2859b113bf479a60f74040f259b0cf85d
ArturRubets <artur.rubets@gmail.com>
2023-05-20 23:33:11 +0300

    File upload

--- laragigs/app/Http/Controllers/ListingController.php
+++ laragigs/app/Http/Controllers/ListingController.php
@@ -31,6 +31,10 @@ class ListingController extends Controller
     {
         $formFields = $request->validate(['title' => 'required', 'company' => ['required', Rule::unique('listings', 'company')], 'location' => 'required', 'website' => 'required', 'email' => ['required', 'email'], 'tags' => 'required', 'description' => 'required']);
 
+        if($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
         Listing::create($formFields);
 
         return redirect('/')->with('message', 'Listing created successfully!');
