24c44c45a1a81c92d29b5c3792c9e15bcaeaf137
ArturRubets <artur.rubets@gmail.com>
2023-05-20 17:25:40 +0300

    Make controllers

--- laragigs/routes/web.php
+++ laragigs/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,11 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 // All Listings
-Route::get('/', function () {
-    return view('listings', ['heading' => 'Latest Listings', 'listings' => Listing::all()]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 // Single Listing
-Route::get('/listings/{listing}', function (Listing $listing) {
-    return view('listing', ['listing' => $listing]);
-});
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
