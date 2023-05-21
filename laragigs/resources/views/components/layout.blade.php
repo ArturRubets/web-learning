5a5d734beea8852e73601c6197971eef8240d349
ArturRubets <artur.rubets@gmail.com>
2023-05-22 00:17:21 +0300

    Change footer

--- laragigs/resources/views/components/layout.blade.php
+++ laragigs/resources/views/components/layout.blade.php
@@ -26,7 +26,7 @@
     <title>LaraGigs | Find Laravel Jobs & Projects</title>
 </head>
 
-<body class="mb-48">
+<body>
     <nav class="flex justify-between items-center mb-4">
         <a href="/"><img class="w-24" src="{{ asset('images/logo.png') }}" alt="" class="logo" /></a>
         <ul class="flex space-x-6 mr-6 text-lg">
@@ -43,10 +43,10 @@
         {{ $slot }}
     </main>
     <footer
-        class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
+        class="w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center relative">
         <p class="ml-2">Copyright &copy; 2023, All Rights reserved</p>
 
-        <a href="/listings/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Post Job</a>
+        <a href="/listings/create" class="absolute right-10 bg-black text-white py-2 px-5 rounded">Post Job</a>
     </footer>
     <x-flash-message />
 </body>
