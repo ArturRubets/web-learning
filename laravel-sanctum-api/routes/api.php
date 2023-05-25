2971209ad6ae5ec951b04730fdd114f3f130f81f
ArturRubets <artur.rubets@gmail.com>
2023-05-25 19:37:20 +0300

    Search product

--- laravel-sanctum-api/routes/api.php
+++ laravel-sanctum-api/routes/api.php
@@ -17,6 +17,8 @@ use App\Http\Controllers\ProductController;
 
 Route::resource('products', ProductController::class);
 
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
