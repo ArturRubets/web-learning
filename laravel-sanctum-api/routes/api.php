5aa848ec03c9f07fa67cc59f45f19b927060ce55
ArturRubets <artur.rubets@gmail.com>
2023-05-26 15:00:57 +0300

    Login user and get token

--- laravel-sanctum-api/routes/api.php
+++ laravel-sanctum-api/routes/api.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\ProductController;
 
 // Public routes
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
