af1b88b46e206029c3ffdbc3929fc00579d41254
ArturRubets <artur.rubets@gmail.com>
2023-06-29 22:43:48 +0300

    Add fonts and slider

---
+++ layouts/di-gi/js/main.js
@@ -0,0 +1,19 @@
+$(function () {
+  $(".top-slider").slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow:
+      '<button type="button" class="slick-arrow slick-next"><div class="arrow-right"></div></button>',
+    prevArrow:
+      '<button type="button" class="slick-arrow slick-prev"><div class="arrow-left"></div></button>',
+    responsive: [
+      {
+        breakpoint: 640,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
+});
