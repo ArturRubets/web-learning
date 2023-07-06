d7b059facc172bee370376435d2ae36f0bf0966a
ArturRubets <artur.rubets@gmail.com>
2023-07-06 14:31:31 +0300

    Format code and reduce size of images

--- layouts/gtcoding-responsive-homepage/main.js
+++ layouts/gtcoding-responsive-homepage/main.js
@@ -5,19 +5,19 @@ const closeIcon = document.querySelector(".mobile-menu-container .close-icon");
 const mobileMenuContainer = document.querySelector(".mobile-menu-container");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 60) {
-        nav.classList.add("scrolled");
-        mobileNav.classList.add("scrolled");
-    } else {
-        nav.classList.remove("scrolled");
-        mobileNav.classList.remove("scrolled");
-    }
+  if (window.scrollY > 60) {
+    nav.classList.add("scrolled");
+    mobileNav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+    mobileNav.classList.remove("scrolled");
+  }
 });
 
 menuIcon.addEventListener("click", () => {
-    mobileMenuContainer.classList.add("active");
+  mobileMenuContainer.classList.add("active");
 });
 
 closeIcon.addEventListener("click", () => {
-    mobileMenuContainer.classList.remove("active");
+  mobileMenuContainer.classList.remove("active");
 });
