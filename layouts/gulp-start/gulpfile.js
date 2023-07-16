398909e425e71786ae1d59c04b204633ee4c966a
ArturRubets <artur.rubets@gmail.com>
2023-07-16 15:09:14 +0300

    Expanding the collection and adding pictures and fonts

--- layouts/gulp-start/gulpfile.js
+++ layouts/gulp-start/gulpfile.js
@@ -6,6 +6,74 @@ const uglify = require("gulp-uglify-es").default;
 const browserSync = require("browser-sync").create();
 const autoprefixer = require("gulp-autoprefixer");
 const clean = require("gulp-clean");
+const avif = require("gulp-avif");
+const webp = require("gulp-webp");
+const imagemin = require("gulp-imagemin");
+const newer = require("gulp-newer");
+const ttf2woff2 = require("gulp-ttf2woff2");
+const svgSprite = require("gulp-svg-sprite");
+const fonter = require("gulp-fonter");
+const rename = require("gulp-rename");
+const include = require("gulp-include");
+
+function pages() {
+  return src("app/pages/*.html")
+    .pipe(
+      include({
+        includePaths: "app/components",
+      })
+    )
+    .pipe(dest("app"))
+    .pipe(browserSync.stream());
+}
+
+function fonts() {
+  return src("app/fonts/src/*.*")
+    .pipe(
+      fonter({
+        formats: ["woff", "ttf"],
+      })
+    )
+    .pipe(src("app/fonts/*.ttf"))
+    .pipe(ttf2woff2())
+    .pipe(
+      rename(function (file) {
+        file.dirname = ""; // Видаляємо папку src з шляху
+        file.basename = file.basename.replace("src\\", ""); // Видаляємо "src\" з імені файлу
+      })
+    )
+    .pipe(dest("app/fonts"));
+}
+
+function images() {
+  return src(["app/images/src/*.*", "!app/images/src/*.svg"])
+    .pipe(newer("app/images/dist"))
+    .pipe(avif({ quality: 50 }))
+
+    .pipe(src("app/images/src/*.*"))
+    .pipe(newer("app/images/dist"))
+    .pipe(webp())
+
+    .pipe(src("app/images/src/*.*"))
+    .pipe(newer("app/images/dist"))
+    .pipe(imagemin())
+    .pipe(dest("app/images/dist"));
+}
+
+function sprite() {
+  return src("app/images/dist/*.svg")
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: "../sprite.svg",
+            example: true,
+          },
+        },
+      })
+    )
+    .pipe(dest("app/images/dist"));
+}
 
 function scripts() {
   return src(["app/js/main.js"])
@@ -25,33 +93,47 @@ function styles() {
 }
 
 function watching() {
-  watch(["app/scss/style.scss"], styles);
-  watch(["app/js/main.js"], scripts);
-  watch(["app/*.html"]).on("change", browserSync.reload);
-}
-
-function browserSyncFunc() {
   browserSync.init({
     server: {
       baseDir: "app/",
     },
   });
+  watch(["app/scss/style.scss"], styles);
+  watch(["app/images/src"], images);
+  watch(["app/js/main.js"], scripts);
+  watch(["app/components/*", "app/pages/*"], pages);
+  watch(["app/*.html"]).on("change", browserSync.reload);
 }
 
 function cleanDist() {
-  return src("dist").pipe(clean());
+  return src("dist", { allowEmpty: true }).pipe(clean());
 }
 
 function building() {
-  return src(["app/css/style.min.css", "app/js/main.min.js", "app/**/*.html"], {
-    base: "app",
-  }).pipe(dest("dist"));
+  return src(
+    [
+      "app/**/*.html",
+      "app/css/style.min.css",
+      "app/images/dist/*.*",
+      "!app/images/dist/*.svg",
+      "!app/images/dist/stack/*",
+      "app/images/dist/sprite.svg",
+      "app/fonts/*.*",
+      "app/js/main.min.js",
+    ],
+    {
+      base: "app",
+    }
+  ).pipe(dest("dist"));
 }
 
 exports.styles = styles;
+exports.images = images;
+exports.fonts = fonts;
+exports.pages = pages;
+exports.sprite = sprite;
 exports.scripts = scripts;
 exports.watching = watching;
-exports.browserSync = browserSyncFunc;
 
 exports.build = series(cleanDist, building);
-exports.default = parallel(styles, scripts, browserSyncFunc, watching);
+exports.default = parallel(styles, images, scripts, pages, watching);
