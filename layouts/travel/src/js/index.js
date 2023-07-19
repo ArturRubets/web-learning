3a8f94244f4aa17e3fdc4ae18a6eb8be2fd532bb
ArturRubets <artur.rubets@gmail.com>
2023-07-19 13:14:30 +0300

    Swiper settings

--- layouts/travel/src/js/index.js
+++ layouts/travel/src/js/index.js
@@ -1,24 +1,27 @@
-// import mobileNav from './modules/mobile-nav.js';
-// mobileNav();
-
 import Swiper from "swiper/bundle";
 import "swiper/css/bundle";
 
 // init Swiper:
 const swiper = new Swiper(".swiper", {
   // Optional parameters
-//   direction: "vertical",
+
   loop: true,
+  parallax: true,
+  speed: 1000,
+  keyboard: {
+    enabled: true,
+  },
 
   // If we need pagination
   pagination: {
-    el: ".swiper-pagination",
+    el: ".slider-control__count",
+    type: "fraction",
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: "#sliderNext",
+    prevEl: "#sliderPrev",
   },
 
   // And if we need scrollbar
