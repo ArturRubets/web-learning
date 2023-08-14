dc4f441cce2aa5b55af7924a38ffa2e155871675
ArturRubets <artur.rubets@gmail.com>
2023-08-14 22:07:44 +0300

    Add trainers slider section

--- layouts/iron-gym/src/js/index.js
+++ layouts/iron-gym/src/js/index.js
@@ -1,2 +1,5 @@
 import mobileNav from "./modules/mobile-nav.js";
+import trainersSlider from "./modules/trainers-slider.js";
+
 mobileNav();
+trainersSlider();
---
+++ layouts/iron-gym/src/js/modules/trainers-slider.js
@@ -0,0 +1,43 @@
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
+
+function trainersSlider() {
+  const swiper = new Swiper(".trainers__slider", {
+    slidesPerView: 1,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+
+    breakpoints: {
+      640: {
+        slidesPerView: 2.5,
+        spaceBetween: 20,
+        slidesOffsetBefore: 40,
+        slidesOffsetAfter: 40,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        slidesOffsetBefore: 78,
+        slidesOffsetAfter: 78,
+      },
+      1024: {
+        slidesPerView: 3.5,
+        spaceBetween: 40,
+        slidesOffsetBefore: 98,
+        slidesOffsetAfter: 98,
+      },
+    },
+
+    pagination: {
+      el: ".trainers__slider-pagination",
+      type: "bullets",
+      bulletClass: "trainers__pagination-bullet", // Вказати клас для кожного квадратика
+      bulletActiveClass: "trainers__pagination-bullet-active", // Вказати клас для активного квадратика
+      clickable: true,
+    },
+  });
+}
+
+export default trainersSlider;
