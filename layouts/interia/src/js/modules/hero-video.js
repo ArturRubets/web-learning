4d0e76e219763e8b4313014a2fab4d00e047f8d6
ArturRubets <artur.rubets@gmail.com>
2023-08-21 21:11:56 +0300

    Animate video hero element

--- layouts/interia/src/js/modules/hero-video.js
+++ layouts/interia/src/js/modules/hero-video.js
@@ -1,7 +1,8 @@
 export default function initializeHeroVideo() {
   document.getElementById("playButton").addEventListener("click", function () {
     const videoElement = document.querySelector(".hero__video");
-    
+    const posterElement = document.querySelector(".hero__poster");
+
     const overlayDiv = document.createElement("div");
     overlayDiv.className = "hero__overlay";
 
@@ -9,8 +10,10 @@ export default function initializeHeroVideo() {
     videoElement.parentNode.insertBefore(overlayDiv, videoElement.nextSibling);
 
     videoElement.play();
-    
+
     // Ховаємо playButton
     this.style.display = "none";
+    // Анімовано ховаємо poster
+    posterElement.style.opacity = 0;
   });
 }
