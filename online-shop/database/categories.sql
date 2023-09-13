420a645300baf7328881010fded275aea2eabb35
“Artur <artur.rubets@gmail.com>
2023-09-13 23:29:39 +0300

    Reorder

--- online-shop/database/categories.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `categories` (id, name, code, description, image, created_at, updated_at)
-VALUES (1, 'Mobile phones', 'mobiles', 'Mobile phones are portable communication devices', 'Image 1', NOW(), NOW()),
-       (2, 'Portable technics', 'portable',
-        'The Portable Technics category offers a wide range of portable electronic devices and gadgets', 'Image 2',
-        NOW(), NOW()),
-       (3, 'Household appliances', 'appliance',
-        'Household appliances encompass a wide range of essential electrical and mechanical devices', 'Image 3', NOW(),
-        NOW());
